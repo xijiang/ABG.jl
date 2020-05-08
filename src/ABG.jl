1c28ff03a7ba27786b575fa1020b1e0726a00054
Xijiang Yu <xijiang@users.noreply.github.com>
2020-05-08 22:30:52 +0200

    export plk cmds

--- src/ABG.jl
+++ src/ABG.jl
@@ -1,6 +1,8 @@
 module ABG
 
-export sandbox_abg
+export ped_n_map_to_bed, extract_bed_subset, miss_allele_stats, allele_maf_stats
+export hwe_stats, plink_merge, plink_2_vcf, vcf_2_plink, bed_2_map_n_ped
+export plink_filter_snp, plink_filter_id
 
 include("styled-messages.jl")
 include("final-reports-2-ped.jl")
